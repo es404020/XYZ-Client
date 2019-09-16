b360adb4cb3b6ac2eaedd412b547e033fcaf4173
pearlsilver35 <pearlsilver35@gmail.com>
2019-09-16 14:29:25 +0100

    project setup

---
+++ js/quixnav-init.js
@@ -0,0 +1,20 @@
+
+(function($) {
+    "use strict"
+
+    new quixSettings({
+        typography: "poppins",
+        version: "light",
+        layout: "vertical",
+        headerBg: "color_1",
+        navheaderBg: "color_2",
+        sidebarBg: "color_2",
+        sidebarStyle: "full",
+        sidebarPosition: "fixed", 
+        headerPosition: "fixed", 
+        containerLayout: "wide",  
+        direction: "ltr" 
+    });
+
+
+})(jQuery);
